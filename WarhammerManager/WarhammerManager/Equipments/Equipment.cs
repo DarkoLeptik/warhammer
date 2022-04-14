f941b08b5e7fa095025f4a0ce6e388d86577a124
Hugo Deniau <hugo.deniau91@gmail.com>
2022-04-14 10:15:35 -0400

    implement interface on Equipment

--- WarhammerManager/WarhammerManager/Equipments/Equipment.cs
+++ WarhammerManager/WarhammerManager/Equipments/Equipment.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WarhammerManager.Rules;
 
 namespace WarhammerManager.Equipments
 {
-    abstract class Equipment
+    abstract class Equipment : Rulable
     {
         string equipmentName;
 
